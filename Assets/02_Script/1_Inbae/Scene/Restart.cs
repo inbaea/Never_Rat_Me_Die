29ee65d0f4b3ed54b6a1a43c6d76d79c7b3834d2
inbaea <49175916+inbaea@users.noreply.github.com>
2023-06-26 20:24:05 +0900

    Changed

--- Assets/02_Script/1_Inbae/Scene/Restart.cs
+++ Assets/02_Script/1_Inbae/Scene/Restart.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour
 {
     public GameObject GameManager;
+    
     void Start()
     {
         GameManager = GameObject.Find("GameManager");
