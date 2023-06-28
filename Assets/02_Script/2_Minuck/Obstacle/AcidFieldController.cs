8ec48fb8722295d08f2665548eb1b37a7e8119a3
AdHoc2675 <alsdnr2675@naver.com>
2023-06-28 21:31:49 +0900

    acidField update

--- Assets/02_Script/2_Minuck/Obstacle/AcidFieldController.cs
+++ Assets/02_Script/2_Minuck/Obstacle/AcidFieldController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AcidFloorController : MonoBehaviour
+public class AcidFieldController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,13 +18,18 @@ public class AcidFloorController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+
+        Debug.Log(other.gameObject.tag);
+
         if (other.transform.tag == "Stone" || other.transform.tag == "WoodBox")
         {
             other.transform.parent.gameObject.SetActive(false);
+            
         }
 
         if (other.transform.tag == "Player")
         {
+            Debug.Log(other.gameObject.tag);
             other.transform.parent.gameObject.transform.Find("PlayerGuide").gameObject.GetComponent<PlayerGuideCorutine>().moveCount = 0;
         }
     }
